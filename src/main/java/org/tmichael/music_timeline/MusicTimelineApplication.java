eba7a4d2f1ac9347340b8f48f7bdc0728d43a41e
tmichael90 <47704941+tmichael90@users.noreply.github.com>
2020-08-24 12:07:54 -0700

    init commit

---
+++ src/main/java/org/tmichael/music_timeline/MusicTimelineApplication.java
@@ -0,0 +1,13 @@
+package org.tmichael.music_timeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicTimelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicTimelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tmichael/music_timeline/MusicTimelineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tmichael.music_timeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicTimelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
